28ab011530f589600ac7682a10290052375fdcb2
Maziar <mazhej>
2019-12-05 17:12:23 -0800

    Cleaning the code

--- hooks.py
+++ hooks.py
@@ -1,4 +1,4 @@
-#https://www.kaggle.com/sironghuang/understanding-pytorch-hooks
+#importing libraries
 import requests
 from PIL import *
 from PIL import Image
@@ -8,7 +8,6 @@ import random
 import shutil
 import time
 import warnings
-
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -22,7 +21,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-# A simple hook class that returns the input and output of a layer during forward/backward pass
+# A simple hook class that returns the input and output of a layer during forward pass
 class Hook():
     def __init__(self, module):
         self.hook = module.register_forward_hook(self.hook_fn)
@@ -35,27 +34,29 @@ class Hook():
             
     def close(self):
         self.hook.remove()
-#######################################################################################
-model = models.resnet50(pretrained=True)
-#################################################################################3
+
+#########################
+#Selecting resnet18 as our model
+model = models.resnet18(pretrained=True)
+
+#########################
 # register hooks on each layer
 hookF = [Hook(layer) for layer in list(model.modules())]
 
+def data_loader():
+    im_object =Image.open("/home/maziar/WA/exampleofhooks/cat_224.jpg")
+    normalize = transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            )
+    to_tensor = transforms.ToTensor()
+    scaler = transforms.Scale((224, 224))
+    data= normalize(to_tensor(scaler(im_object))).unsqueeze(0)
+    return data
 
-im_object =Image.open("/home/maziar/WA/exampleofhooks/cat_224.jpg")
-normalize = transforms.Normalize(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        )
-to_tensor = transforms.ToTensor()
-scaler = transforms.Scale((224, 224))
-dataa = normalize(to_tensor(scaler(im_object))).unsqueeze(0)
 
+out = model(data_loader())
 
-out = model(dataa)
-
-#out.backward(torch.tensor([1,1],dtype=torch.float),retain_graph=True)
-#! loss.backward(retain_graph=True)  # doesn't work with backward hooks, 
-#! since it's not a network layer but an aggregated result from the outputs of last layer vs target 
+ 
 
 print('***'*3+'  Forward Hooks Inputs & Outputs  '+'***'*3)
 tot_num_ops = 0
